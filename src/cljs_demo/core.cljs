05e8fb5601814af8812d6d8f3c22392857873fdd
Ory Band <oryband@users.noreply.github.com>
2022-08-24 09:07:48 +0100

    First Draft

---
+++ src/cljs_demo/core.cljs
@@ -0,0 +1,18 @@
+(ns cljs-demo.core
+  (:require ["color" :as color]))
+
+(defn my-color []
+  (-> (color "#7743CE")
+      (.alpha 0.5)
+      (.lighten 0.5)
+      (.hsl)))
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn main []
+  (println "Cljs is awesome!" (str (my-color))))
+
+(comment
+  (-> (color "#7743CE")
+      (.alpha 0.5)
+      (.lighten 0.5)
+      (.hsl)))
